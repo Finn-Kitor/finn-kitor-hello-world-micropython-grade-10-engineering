81ed5e3e1658a93589609beb6767683adb242ca8
Finn Kitor <118993032+Finn-Kitor@users.noreply.github.com>
2022-11-29 10:28:40 -0500

    Add files via upload

---
+++ hello world-main (2).py
@@ -0,0 +1,10 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image.HAPPY)
+    sleep(1000)
+    display.scroll('Hello Finn!')
+    
